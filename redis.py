45f265a0bc15b0cf7f14424a1d807e06a8cf33dd
simongemmill <95191683+simongemmill@users.noreply.github.com>
2025-09-28 23:36:19 +0300

    Update redis.py
    commit

--- redis.py
+++ redis.py
@@ -3,8 +3,9 @@ import redis
 
 # Connect to your Key Value instance using the REDIS_URL environment variable
 # The REDIS_URL is set to the internal connection URL e.g. redis://red-343245ndffg023:6379
-r = redis.from_url(os.environ['REDIS_URL'])
+r = redis.from_url(os.environ['rediss://red-d3cfita4d50c73cgu7kg:fd5oeGXHR8AcGIbJBwkIC9S4wXET3kRJ@oregon-keyvalue.render.com:6379'])
 
 # Set and retrieve some values
 r.set('key', 'redis-py')
+
 print(r.get('key').decode())
